4938f61933783689f395bd0b1f77323a6a47711b
SilvrAdam <104857589+SilvrAdam@users.noreply.github.com>
2022-05-04 00:30:52 -0400

    Add files via upload

---
+++ Crystal Caverns/src/entities/Crabby.java
@@ -0,0 +1,14 @@
+package entities;
+
+import static Utilz.Constants.EnemyConstants.*;
+
+import Main.Game;
+
+public class Crabby extends Enemy {
+
+	public Crabby(float x, float y) {
+		super(x, y, CRABBY_WIDTH, CRABBY_HEIGHT, CRABBY);
+
+	}
+
+}
